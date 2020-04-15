c9c60d7954b3cdcd24a59ebfb481f1e84cbfbe4b
Jeferson Amorim <jeferson.amorim@live.com>
2020-04-14 21:47:54 -0300

    Implementação da pagina feed

--- blogPessoal/src/app/feed/feed.component.ts
+++ blogPessoal/src/app/feed/feed.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedComponent implements OnInit {
 
+  myimage:string ='assets/images/eu.jpg'
   constructor() { }
 
   ngOnInit(): void {
